1f856e9d61f6bd2212b6e23e5e914bf9e981c475
cáceres476 <alakycm@hotmail.com>
2025-04-02 22:07:02 -0600

    Comentarios en el login
    Explicación de los contenedores en el login

--- LOGIN/script.js
+++ LOGIN/script.js
@@ -1,13 +1,19 @@
+// Selecciona el formulario con la clase "my-form"
 let loginForm = document.querySelector(".my-form");
 
+// Agrega un único evento "submit" al formulario
 loginForm.addEventListener("submit", (e) => {
+    // Previene el comportamiento predeterminado del formulario (recargar la página)
     e.preventDefault();
-    let email = document.getElementById("email");
+
+    // Obtiene el valor del campo de entrada con id "username"
+    let username = document.getElementById("username"); 
+    // Obtiene el valor del campo de entrada con id "password"
     let password = document.getElementById("password");
 
-    console.log('Email:', email.value);
+    // Muestra en la consola los valores ingresados en los campos de username y password
+    console.log('Username:', username.value);
     console.log('Password:', password.value);
-    // process and send to API 
+
+    // Aquí se procesarían los datos y se enviarían a una API
 });
-
-
